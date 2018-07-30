415a77cf6e33a0b8f1ba91775a213f6a98c4c882
jordan-massingill <jordan_massingill@hotmail.com>
2018-07-30 14:11:54 -0500

    A few extra changes

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import './App.css';
 class App extends Component {
   constructor() {
     super();
-    this.state: = {
-      Avengers: []
+    this.state = {
+      avengers: ['Iron Man', 'Thor', 'Hank Pym', 'Wasp', 'Hulk', 'Captain America', 'Hawkeye', 'Quicksilver']
     }
   }
 
